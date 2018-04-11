69b8fee188df458733095f5bf8a6e81ca5e15fd7
Waihon Yew <yewwaihon@gmail.com>
2018-04-11 23:15:00 +0800

    Initial Commit

---
+++ The Night Porter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Night Porter
+//
+//  Created by Waihon Yew on 11/04/2018.
+//  Copyright © 2018 Pringgle Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
